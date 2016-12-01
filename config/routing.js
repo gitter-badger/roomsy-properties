fe5b2880c1a0b8042d9ecc2853bf596ae5ccc0c4
Tuan Nguyen <nmtuan.dev@gmail.com>
2016-12-01 11:48:18 +0700

    Add online status confirmation

--- config/routing.js
+++ config/routing.js
@@ -6,6 +6,7 @@ var apiRouter = Utils.getRouter('api');
 //------------------------------------------------------------------------
 var appRouter = express.Router();
 appRouter.use('/api', apiRouter);
+appRouter.get('/', (req, res) => {res.end('roomsy-properties is online')});
 
 //------------------------------------------------------------------------
 var configObject = {
